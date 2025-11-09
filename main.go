6c68f8ed3627a67ae574f2349d4a9c0fbd484822
robiverdev <robi.verdev1@gmail.com>
2025-11-09 18:21:39 +0100

    add pokeapi client and types for location areas

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/robiverdev/pokedexcli/internal/pokeapi"
+)
+
 func main() {
-	startRepl()
+	pokeapiClient := pokeapi.NewClient()
+	resp, err := pokeapiClient.ListLocationAreas()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp)
+
+	// startRepl()
 }
